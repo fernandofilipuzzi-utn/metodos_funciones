e9b236b97808b8f07261a0a61e1fa121fe806fe6
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2024-05-14 08:52:36 -0300

    iniciando repositorio

---
+++ GenerarNumerosPrimos/GenerarNumerosPrimos/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerarNumerosPrimos
+{
+    class Program
+    {
+
+        //resuelvo si un numero es primo
+        public static bool EsPrimo(int num)
+        {
+            int divisibilidad;
+            bool retorno;
+
+            divisibilidad = 0;
+            for (int divisor = 1; divisor <= num; divisor++)
+            {
+                if (num % divisor == 0)
+                {
+                    divisibilidad++;
+                }
+            }
+
+            //esto es mejorable, cómo?
+            if (divisibilidad <= 2)
+            {
+                retorno = true;
+            }
+            else
+            {
+                retorno = false;
+            }
+            return retorno;
+            //fin dela posible mejora?
+        }
+
+        static void Main(string[] args)
+        {
+            int desdeNum, hastaNum;
+            bool resultado;
+
+            Console.WriteLine("Ingrese el intervalo");
+            desdeNum = Convert.ToInt32(Console.ReadLine());
+            hastaNum = Convert.ToInt32(Console.ReadLine());
+
+            for (int num = desdeNum; num <= hastaNum; num++)
+            {
+                resultado = EsPrimo(num);
+
+                if (resultado)
+                {
+                    Console.WriteLine(num);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+}
